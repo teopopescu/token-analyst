5c4be9860d7ee5cb39c87e002c748dc998b1ed08
teopopescu <teodor.popescu94@yahoo.com>
2019-03-06 14:46:19 +0000

    tidy up code into class, changed test for extract-transform flow

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 from price_pipeline import price_pipeline
 from transform_data import transform_data
 
-test = price_pipeline()
-#test.extract_historical_data('ZRX',1502928000)
-
-#test.transform_data('ZRX')
+test_extract = price_pipeline()
+test_extract.extract_historical_data('ZRX',1502928000)
 
 test_transform = transform_data()
 test_transform.serialize_records(test_transform.read_price_data('ZRX.csv'))
--- transform_data.py
+++ transform_data.py
@@ -4,44 +4,48 @@ from avro.io import DatumReader, DatumWriter
 import csv
 from collections import namedtuple
 
-#class transform_data():
+class transform_data():
 
-file_path = "ZRX.csv"
-fields = ("close", "high", "low", "open", "time", "volumefrom", "volumeto", "pair")
-priceStreamRecord = namedtuple('record', fields)
+    fields = ("close", "high", "low", "open", "time", "volumefrom", "volumeto", "pair")
+    priceStreamRecord = namedtuple('record', fields)
 
-def parse_price_data(row):
-    return {
-        "close": float(row[0]),
-        "high": float(row[1]),
-        "low": float(row[2]),
-        "open": float(row[3]),
-        "time": int(row[4]),
-        "volumefrom": float(row[5]),
-        "volumeto": float(row[6]),
-        "pair": str(row[7])
-        }
-
-def read_price_data(path):
-    with open(path, 'rU') as data:
-        data.readline()
-        reader = csv.reader(data, delimiter = ",")
-        list_of_rows=[]
-        for row in reader:
-            yield parse_price_data(row)
+    @staticmethod
+    def parse_price_data(row):
+        return {
+            "close": float(row[0]),
+            "high": float(row[1]),
+            "low": float(row[2]),
+            "open": float(row[3]),
+            "time": int(row[4]),
+            "volumefrom": float(row[5]),
+            "volumeto": float(row[6]),
+            "pair": str(row[7])
+            }
 
 
-def parse_schema(path="price_schema.avsc"):
-    with open(path, 'r') as schema:
-        return avro.schema.Parse(schema.read())
+    @staticmethod
+    def read_price_data(path):
+        transformer = transform_data()
+        with open(path, 'rU') as data:
+            data.readline()
+            reader = csv.reader(data, delimiter = ",")
+            list_of_rows=[]
+            for row in reader:
+                yield transformer.parse_price_data(row)
 
+    @staticmethod
+    def parse_schema(path="price_schema.avsc"):
+        with open(path, 'r') as schema:
+            return avro.schema.Parse(schema.read())
 
-def serialize_records(records, avro_output="ZRX.avro"):
-    schema = parse_schema()
-    with open(avro_output, 'wb') as out:
-        writer = DataFileWriter(out, DatumWriter(), schema)
-        for record in records:
-            writer.append(record)
+    @staticmethod
+    def serialize_records(records, avro_output="ZRX.avro"):
+        transformer = transform_data()
+        schema = transformer.parse_schema()
+        with open(avro_output, 'wb') as out:
+            writer = DataFileWriter(out, DatumWriter(), schema)
+            for record in records:
+                writer.append(record)
 
-serialize_records(read_price_data('ZRX.csv'))
+    #serialize_records(read_price_data('ZRX.csv'))
 
