e31c4c8417d36ff0beb9f74461a511facfd876d0
teopopescu <teodor.popescu94@yahoo.com>
2019-03-06 14:14:55 +0000

    deleted .idea and log file, removed failing test

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 from price_pipeline import price_pipeline
-#from transform_data import transform_data
+from transform_data import transform_data
 
 test = price_pipeline()
 #test.extract_historical_data('ZRX',1502928000)
 
-test.transform_data('ZRX')
+#test.transform_data('ZRX')
 
-#test_transform = transform_data()
-#test_transform.serialize_records(test_transform.read_price_data('ZRX.csv'))
+test_transform = transform_data()
+test_transform.serialize_records(test_transform.read_price_data('ZRX.csv'))
