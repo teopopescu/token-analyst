ccdd2781348d501081aadb7e81bd5fe272eb71d1
teopopescu <teodor.popescu94@yahoo.com>
2019-03-13 15:03:48 +0000

    final accounts data scraper version

--- ethScraper.py
+++ ethScraper.py
@@ -31,24 +31,33 @@ class ethScraper():
         html = response.content
         soup = BeautifulSoup(html, 'html.parser')
         cells = soup.find_all('td')
-        cell_text = []
-        for cell in cells:
-            cell_text.append(cell.get_text())
-        addresses = cell_text[0::4]
-        labels = cell_text[1::4]
-        etherscan_data = pd.DataFrame({'Adresses': addresses, 'Labels': labels})
-        etherscan_data['Classification'] = classification_name
-        etherscan_data['Source'] = 'Etherscan'
-        etherscan_data['Type'] = 'Account'
+        if len(cells) > 1:
+            cell_text = []
+            for cell in cells:
+                cell_text.append(cell.get_text())
+            addresses = cell_text[0::4]
+            labels = cell_text[1::4]
+            etherscan_data = pd.DataFrame({'Address': addresses, 'Labels': labels})
+            etherscan_data['Classification'] = classification_name
+            etherscan_data['Source'] = 'Etherscan'
+            etherscan_data['Type'] = 'Account'
+        else:
+            etherscan_data = pd.DataFrame([['No address', 'No labels', classification_name, 'Etherscan', 'Account']],
+                                          columns=['Address', 'Labels', 'Classification', 'Source', 'Type'])
         return etherscan_data
+
 
 if __name__  ==  "__main__":
     classification = ethScraper.extract_classifications()
     ethScraper.extract_accounts_data(classification[0]).to_csv('accounts_data.csv', index=False)
     with open('accounts_data.csv', 'a') as f:
-        for item in classification[1:]:
-            extracted_data = ethScraper.extract_accounts_data(item)
-            if extracted_data.empty:
-                next(item, None)
+        #for item in classification[1:]:
+        i = 0
+        while i < len(classification[1:]):
+            #extracted_data = ethScraper.extract_accounts_data(item)
+            extracted_data = ethScraper.extract_accounts_data(classification[1:][i])
+            if extracted_data['Address'].empty:
+               i+=1
             else:
                 extracted_data.to_csv(f, header=False)
+                i+=1
